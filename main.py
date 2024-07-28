6a65e2e590774155d93bfc86e84e1664d7744f57
harry843 <63357049+harry843@users.noreply.github.com>
2024-07-28 23:10:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,23 @@ def main():
     data = load_reference()
     env_path = data["env_path"]
     load_environment(env_path)
-    
+
+    # Set variables from reference.json config
+    debit_categories = data["debit_categories"]
     credit_statement_location = data["credit_statement_location"]
     files = list_files(credit_statement_location)
 
+    # Convert unstructured pdf statements to structured data
     df = credit_transactions_to_df(files, credit_statement_location)
     debit_transactions = df[df['Credit Amount'].isna()]['Transaction Description'].tolist()
 
-    debit_categories = data["debit_categories"]
-    
+    # Query ChatGPT to map transaction descriptions to spend categories
     credit_card_category_mappings = send_multiple_chatGPT_queries(data["first_prompt"], debit_transactions , debit_categories, data["follow_up_prompt"])
 
+    # Merge structured data with ChatGPT generated spend categories
     final_df = merge_mapping_with_transactions(credit_card_category_mappings, df)
 
+    # Export final output to csv
     final_df.to_csv(r"finances_categorised.csv",index=False)
 
 if __name__ == "__main__":
